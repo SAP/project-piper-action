f514ae07ff5308dbe026f2b4ec8d853348c4556a
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-02-13 11:26:56 +0100

    Add flags to actions definition

--- dist/index.js
+++ dist/index.js
@@ -959,14 +959,10 @@ const fs = __webpack_require__(747);
 async function run() {
   try {
     const piperPath = await tc.downloadTool('https://github.com/SAP/jenkins-library/releases/download/v1.12.0/piper');
-    fs.chmod(piperPath, 0o775, (err) => {
-      if (err) throw err;
-      console.log('Piper is executable');
-    })
-    const command = core.getInput('command');
-    console.log(`running piper ${piperPath} with command ${command}`)
-
-    await exec.exec(`${piperPath} ${command}`);
+    fs.chmodSync(piperPath, 0o775)
+    const command = core.getInput('command')
+    const flags = core.getInput('flags')
+    await exec.exec(`${piperPath} ${command} ${flags}`);
   }
   catch (error) {
     core.setFailed(error.message);
