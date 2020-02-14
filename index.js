dbea63422f5bf3e6df9cddc74c12f755a7b9fafb
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-02-14 16:42:04 +0100

    Run tests on GitHub Actions

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const core = require('@actions/core');
-const tc = require('@actions/tool-cache');
-const exec = require('@actions/exec');
-const fs = require('fs');
+const core = require('@actions/core')
+const tc = require('@actions/tool-cache')
+const exec = require('@actions/exec')
+const fs = require('fs')
 
-async function run() {
+async function run () {
   try {
     const piperPath = await tc.downloadTool('https://github.com/SAP/jenkins-library/releases/latest/download/piper')
     fs.chmodSync(piperPath, 0o775)
     const command = core.getInput('command')
     const flags = core.getInput('flags')
     await exec.exec(`${piperPath} ${command} ${flags}`)
-  }
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message)
   }
 }
