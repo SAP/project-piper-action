fbc6ec0c64cc4a9f0ca5ef76b5ed76b6a9324edc
Christopher Fenner <Christopher.Fenner@sap.com>
2020-02-19 23:07:16 +0100

    fix test case

--- test/piper.test.js
+++ test/piper.test.js
@@ -1,34 +1,33 @@
 jest.mock('@actions/core');
 jest.mock('@actions/exec')
 jest.mock('@actions/tool-cache')
-//jest.mock('fs')
 
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache')
 const exec = require('@actions/exec')
-//const fs = require('fs')
+const fs = require('fs')
 
 const run = require('../src/piper.js');
 
 /* eslint-disable no-undef */
 describe('Piper', () => {
-  beforeEach(() => {})
-  afterEach(() => {})
+  beforeEach(() => {
+    tc.downloadTool
+      .mockReturnValue('./piper')
+    core.getInput
+      .mockReturnValueOnce('version')
+      .mockReturnValueOnce('--noTelemetry')
+
+      fs.chmodSync = jest.fn()
+  })
+  afterEach(() => {
+  })
 
   test('', async () => {
-    tc.downloadTool = jest
-      .fn()
-      .mockReturnValueOnce('myToolInLocalDirectory')
-    core.getInput = jest
-      .fn()
-      .mockReturnValueOnce('command')
-      .mockReturnValueOnce('flags')
-      
-//    fs.chmodSync = jest.fn()
-    exec.exec = jest.fn()
 
     await run();
 
-    expect(exec.exec).toHaveBeenCalledWith('myToolInLocalDirectory command flags');
+    expect(fs.chmodSync).toHaveBeenCalledWith('./piper', 0o775);
+    expect(exec.exec).toHaveBeenCalledWith('./piper version --noTelemetry');
   });
 });
