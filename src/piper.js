d193c4f9d85b57aae019647d786a5b873a85ddab
Christopher Fenner <Christopher.Fenner@sap.com>
2020-02-19 21:57:15 +0100

    add test case

--- src/piper.js
+++ src/piper.js
@@ -6,7 +6,7 @@ const fs = require('fs')
 async function run () {
   try {
     const piperPath = await tc.downloadTool('https://github.com/SAP/jenkins-library/releases/latest/download/piper')
-    fs.chmodSync(piperPath, 0o775)
+    //fs.chmodSync(piperPath, 0o775)
     const command = core.getInput('command')
     const flags = core.getInput('flags')
     await exec.exec(`${piperPath} ${command} ${flags}`)
--- test/piper.test.js
+++ test/piper.test.js
@@ -1,3 +1,34 @@
+jest.mock('@actions/core');
+jest.mock('@actions/exec')
+jest.mock('@actions/tool-cache')
+//jest.mock('fs')
 
-test('no test yet', async() => {
+const core = require('@actions/core');
+const tc = require('@actions/tool-cache')
+const exec = require('@actions/exec')
+//const fs = require('fs')
+
+const run = require('../src/piper.js');
+
+/* eslint-disable no-undef */
+describe('Piper', () => {
+  beforeEach(() => {})
+  afterEach(() => {})
+
+  test('', async () => {
+    tc.downloadTool = jest
+      .fn()
+      .mockReturnValueOnce('myToolInLocalDirectory')
+    core.getInput = jest
+      .fn()
+      .mockReturnValueOnce('command')
+      .mockReturnValueOnce('flags')
+      
+//    fs.chmodSync = jest.fn()
+    exec.exec = jest.fn()
+
+    await run();
+
+    expect(exec.exec).toHaveBeenCalledWith('myToolInLocalDirectory command flags');
+  });
 });
