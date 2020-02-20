86bdea6bea28e59f0a5efdd0b3b4361c83410eec
Christopher Fenner <Christopher.Fenner@sap.com>
2020-02-20 13:54:07 +0100

    make command mandatory

--- src/piper.js
+++ src/piper.js
@@ -5,10 +5,11 @@ const fs = require('fs')
 
 async function run () {
   try {
+    const command = core.getInput('command', { required: true })
+    const flags = core.getInput('flags')
+
     const piperPath = await tc.downloadTool('https://github.com/SAP/jenkins-library/releases/latest/download/piper')
     fs.chmodSync(piperPath, 0o775)
-    const command = core.getInput('command')
-    const flags = core.getInput('flags')
     await exec.exec(`${piperPath} ${command} ${flags}`)
   } catch (error) {
     core.setFailed(error.message)
