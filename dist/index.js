dc76dc1f4eae7797fa81ec9c330166d573215693
Christopher Fenner <Christopher.Fenner@sap.com>
2020-02-20 16:32:53 +0100

    update dist

--- dist/index.js
+++ dist/index.js
@@ -4457,10 +4457,12 @@ const fs = __webpack_require__(747)
 
 async function run () {
   try {
+    const command = core.getInput('command', { required: true })
+    const flags = core.getInput('flags')
+
+    console.log('Loading piper as latest release')
     const piperPath = await tc.downloadTool('https://github.com/SAP/jenkins-library/releases/latest/download/piper')
     fs.chmodSync(piperPath, 0o775)
-    const command = core.getInput('command')
-    const flags = core.getInput('flags')
     await exec.exec(`${piperPath} ${command} ${flags}`)
   } catch (error) {
     core.setFailed(error.message)
