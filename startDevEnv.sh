553ae548b2ce6326e019f3c70c7c42641d0f0b52
Florian Wilhelm <florian.wilhelm02@sap.com>
2020-08-07 16:52:08 +0200

    update

--- startDevEnv.sh
+++ startDevEnv.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-npm install
-npm run package
+npm i -g @zeit/ncc
+./gradlew build
+ncc build build/js/packages/project-piper-action/kotlin/project-piper-action.js  -o dist
 wget --timestamping https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz
 docker build . -f DevEnv.Dockerfile -t project-piper-action
 docker run -it --rm project-piper-action bash
