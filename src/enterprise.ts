055f2bd2390d5adf5fa3aeed2976db9300baa630
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2023-10-11 19:49:09 +0600

    Adapt for usage of different config files for TOOLS and WDF

--- src/enterprise.ts
+++ src/enterprise.ts
@@ -1,8 +1,11 @@
 import { GITHUB_COM_SERVER_URL } from './github'
 
 export const ENTERPRISE_DEFAULTS_FILENAME = 'piper-defaults.yml'
-export const ENTERPRISE_STAGE_CONFIG_FILENAME = 'github-stage-config.yml'
+// to be reverted once stage config files in piper-library are aligned 
+export const WDF_ENTERPRISE_STAGE_CONFIG_FILENAME = 'github-stage-config.yml'
+export const ENTERPRISE_STAGE_CONFIG_FILENAME = 'piper-stage-config.yml'
 
+const GITHUB_WDF_SERVER_URL = 'https://github.wdf.sap.corp'
 const ENTERPRISE_STEPNAME_PREFIX = 'sap'
 
 export function isEnterpriseStep (stepName: string): boolean {
@@ -26,7 +29,18 @@ export function getEnterpriseDefaultsUrl (owner: string, repository: string): st
 
 export function getEnterpriseStageConfigUrl (owner: string, repository: string): string {
   if (onGitHubEnterprise() && owner !== '' && repository !== '') {
+    if (onGitHubWDF()) {
+      return `${process.env.GITHUB_API_URL}/repos/${owner}/${repository}/contents/resources/${WDF_ENTERPRISE_STAGE_CONFIG_FILENAME}`
+    }
     return `${process.env.GITHUB_API_URL}/repos/${owner}/${repository}/contents/resources/${ENTERPRISE_STAGE_CONFIG_FILENAME}`
   }
   return ''
+}
+
+// to be deleted once stage config files in piper-library are aligned 
+function onGitHubWDF(): boolean {
+  if (process.env.GITHUB_SERVER_URL === GITHUB_WDF_SERVER_URL) {
+    return true
+  }
+  return false
 }
