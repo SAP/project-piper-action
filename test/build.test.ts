4c5de97b37423db1fba331b696bbc150d302a043
Valentin <hi@valentino.dev>
2025-01-06 15:49:46 +0200

    test

--- test/build.test.ts
+++ test/build.test.ts
@@ -7,8 +7,8 @@ describe('parseDevelVersion', () => {
     const { env, owner, repository, commitISH } = parseDevelVersion(version)
     expect(env).toBe('devel')
     expect(owner).toBe('GH_OWNER')
-    expect(repository).toBe('REPOSITORY')
     expect(commitISH).toBe('COMMITISH')
+    expect(repository).toBe('REPOSITORY')
   })
 
   it('should throw an error for an invalid version string', () => {
