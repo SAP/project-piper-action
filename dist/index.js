6b002f2d1e6e21becaf483d36ddee83bcb85dc8f
Gulom Alimov <gulomjon.alimov@sap.com>
2025-01-16 17:07:00 +0200

    update dist folder

--- dist/index.js
+++ dist/index.js
@@ -20495,15 +20495,18 @@ function tokenize(input) {
     // This regular expression looks for:
     // 1. Sequences inside double quotes which may contain spaces (captured including the quotes)
     // 2. Or sequences of non-space characters
-    const argRegex = /"[^"]*"|\S+/g;
-    const matches = input.match(argRegex);
-    return matches ? matches.map(arg => {
-        // Preserve the double quotes around arguments that contain spaces
+    const regex = /"[^"]*"|\S+/g;
+    const matches = input.match(regex);
+    if (matches == null) {
+        return [];
+    }
+    return matches.map(arg => {
+        // Preserve the double quotes around arguments
         if (arg.startsWith('"') && arg.endsWith('"')) {
             return arg;
         }
         return arg;
-    }) : [];
+    });
 }
 exports.tokenize = tokenize;
 
