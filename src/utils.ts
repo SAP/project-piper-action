88e445434b743ba31db21786e714d5d8962072d0
Gulom Alimov <gulomjon.alimov@sap.com>
2025-01-16 17:07:07 +0200

    eslint fix

--- src/utils.ts
+++ src/utils.ts
@@ -4,14 +4,19 @@ export function tokenize (input: string): string[] {
   // This regular expression looks for:
   // 1. Sequences inside double quotes which may contain spaces (captured including the quotes)
   // 2. Or sequences of non-space characters
-  const argRegex = /"[^"]*"|\S+/g
+  const regex = /"[^"]*"|\S+/g
 
-  const matches = input.match(argRegex)
-  return matches ? matches.map(arg => {
-    // Preserve the double quotes around arguments that contain spaces
+  const matches = input.match(regex)
+  if (matches == null) {
+    return []
+  }
+
+  return matches.map(arg => {
+    // Preserve the double quotes around arguments
     if (arg.startsWith('"') && arg.endsWith('"')) {
       return arg
     }
+
     return arg
-  }) : []
+  })
 }
